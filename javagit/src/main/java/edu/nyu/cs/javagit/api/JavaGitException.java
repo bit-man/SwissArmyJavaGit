2a28026b8d10eac01d7e9c17e839acc72a212b3a
jhl388 <jhl388@7bfa78df-70c8-4e7b-bb4f-2d2f1591e478>
2008-06-16 17:04:41 +0000

    - Adding a base exception class for javagit specific exceptions.

---
+++ javagit/src/main/java/edu/nyu/cs/javagit/api/JavaGitException.java
@@ -0,0 +1,69 @@
+package edu.nyu.cs.javagit.api;
+
+/**
+ * Base exception for git specific exceptions.
+ */
+public class JavaGitException extends Exception {
+
+	/** The code for an exception instance. */
+	private int code;
+
+	/**
+	 * Create an exception with a code.
+	 * 
+	 * @param code
+	 *            The code for this exception.
+	 */
+	public JavaGitException(int code) {
+		this.code = code;
+	}
+
+	/**
+	 * Create an exception with a code and a message.
+	 * 
+	 * @param code
+	 *            The code for this exception.
+	 * @param message
+	 *            The message for this exception.
+	 */
+	public JavaGitException(int code, String message) {
+		super(message);
+		this.code = code;
+	}
+
+	/**
+	 * Create an exception with a code, a message and a causal
+	 * <code>Throwable</code>.
+	 * 
+	 * @param code
+	 *            The code for this exception.
+	 * @param message
+	 *            The message for this exception.
+	 * @param cause
+	 *            A <code>Throwable</code> that caused this exception.
+	 */
+	public JavaGitException(int code, String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * Create an exception with a code and a causal <code>Throwable</code>.
+	 * 
+	 * @param code
+	 *            The code for this exception.
+	 * @param cause
+	 *            A <code>Throwable</code> that caused this exception.
+	 */
+	public JavaGitException(int code, Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * Get the code this exception is thrown with.
+	 * 
+	 * @return The code for this exception.
+	 */
+	public int getCode() {
+		return code;
+	}
+}
