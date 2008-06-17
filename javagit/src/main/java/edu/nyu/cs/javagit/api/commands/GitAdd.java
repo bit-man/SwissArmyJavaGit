57d7d1f8910ef3e23b855e9ca134e2a7f233c6f5
gsd216 <gsd216@7bfa78df-70c8-4e7b-bb4f-2d2f1591e478>
2008-06-17 22:11:33 +0000

    GitAdd interface

---
+++ javagit/src/main/java/edu/nyu/cs/javagit/api/commands/GitAdd.java
@@ -0,0 +1,13 @@
+package edu.nyu.cs.javagit.api.commands;
+
+/**
+ * <code>GitAdd</code> provides an interface for adding files or 
+ * directory tree into git repository
+ */
+public class GitAdd {
+	
+	public GitAddResponse add( String repositoryPath, String fileName ) {
+		return null;
+	}
+
+}
