37d49cbc88ad520b0ef09882e782d3bf9cc6c289
gsd216 <gsd216@7bfa78df-70c8-4e7b-bb4f-2d2f1591e478>
2008-06-17 22:13:02 +0000

    First cut of git-add command for cli

--- javagit/src/main/java/edu/nyu/cs/javagit/client/IGitAdd.java
+++ javagit/src/main/java/edu/nyu/cs/javagit/client/IGitAdd.java
@@ -1,10 +1,23 @@
 package edu.nyu.cs.javagit.client;
 
+import edu.nyu.cs.javagit.api.commands.GitAddResponse;
+import java.io.IOException;
+
 /**
  * An interface to represent the git-add command.
  */
 public interface IGitAdd {
 
-	// TODO: Create method signatures for this interface.
+	/**
+	 * 
+	 * @param repositoryPath
+	 * @param args
+	 * @return
+	 * @throws IOException can be thrown because
+	 * 	- The path to filename does not exist
+	 *  - or the file does not exist
+	 *  - repository path does not exist
+	 */
+	public GitAddResponse add( String directory, String[] args ) throws IOException;
 
 }
