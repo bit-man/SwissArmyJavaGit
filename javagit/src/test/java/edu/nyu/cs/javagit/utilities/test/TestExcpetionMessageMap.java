3d041f379c7c0c77970b791f0ea3b2fdf9b06a5f
jhl388 <jhl388@7bfa78df-70c8-4e7b-bb4f-2d2f1591e478>
2008-06-22 16:50:04 +0000

    Adding a set of unit tests for testing ExceptionMessageMap.java

---
+++ javagit/src/test/java/edu/nyu/cs/javagit/utilities/test/TestExcpetionMessageMap.java
@@ -0,0 +1,33 @@
+package edu.nyu.cs.javagit.utilities.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.nyu.cs.javagit.utilities.ExceptionMessageMap;
+
+import junit.framework.TestCase;
+
+/**
+ * Test class for edu.nyu.cs.javagit.utiltites.ExceptionMessageMap.
+ */
+public class TestExcpetionMessageMap extends TestCase {
+
+  @Before
+  protected void setup() {
+  }
+
+  @Test
+  public void testGetMessage() {
+    assertGetMessageValid(null, "NO MESSAGE FOR ERROR CODE. { code=[null] }");
+    assertGetMessageValid("", "NO MESSAGE FOR ERROR CODE. { code=[] }");
+    assertGetMessageValid("0", "NO MESSAGE FOR ERROR CODE. { code=[0] }");
+    assertGetMessageValid("000001", "000001: A String variable was not specified but is required.");
+  }
+
+  private void assertGetMessageValid(String code, String expectedMessage) {
+    String actualMessage = ExceptionMessageMap.getMessage(code);
+    assertEquals("Expected message was not received from ExcpetionMessageMap.getMessage()",
+        expectedMessage, actualMessage);
+  }
+
+}
