6a1319fc0b2edb2f2a95ae6b923c064fc8f51a1d
jhl388 <jhl388@7bfa78df-70c8-4e7b-bb4f-2d2f1591e478>
2008-06-30 00:36:28 +0000

    Alphabetizing methods in these classes.

--- javagit/src/main/java/edu/nyu/cs/javagit/utilities/CheckUtilities.java
+++ javagit/src/main/java/edu/nyu/cs/javagit/utilities/CheckUtilities.java
@@ -10,68 +10,18 @@ import java.util.List;
 public class CheckUtilities {
 
   /**
-   * Checks to see if two objects are equal. The Object.equal() method is used to check for
-   * equality.
+   * Checks that the specified filename exists. This assumes that the above check for string
+   * validity has already been run and the path/filename is neither null or of size 0.
    * 
-   * @param o1
-   *          The first object to check.
-   * @param o2
-   *          The second object to check.
-   * @return True if the two objects are equal. False if the objects are not equal.
+   * @param filename
+   *          File or directory path
    */
-  public static boolean checkObjectsEqual(Object o1, Object o2) {
-    if (null != o1 && !o1.equals(o2)) {
-      return false;
+  public static void checkFileValidity(String filename) throws IOException {
+    File file = new File(filename);
+    if (!file.exists()) {
+      throw new IOException(ExceptionMessageMap.getMessage("020001") + "  { filename=[" + filename
+          + "] }");
     }
-
-    if (null == o1 && null != o2) {
-      return false;
-    }
-
-    return true;
-  }
-
-  /**
-   * Checks if two unordered lists are equal.
-   * 
-   * @param l1
-   *          The first list to test.
-   * @param l2
-   *          The second list to test.
-   * @return True if:
-   *         <ul>
-   *         <li>both lists are null or</li>
-   *         <li>both lists are the same length, there exists an equivalent object in l2 for all
-   *         objects in l1, and there exists an equivalent object in l1 for all objects in l2</li>
-   *         </ul>
-   *         False otherwise.
-   */
-  public static boolean checkUnorderedListsEqual(List<?> l1, List<?> l2) {
-    if (null == l1 && null != l2) {
-      return false;
-    }
-
-    if (null != l1 && null == l2) {
-      return false;
-    }
-
-    if (l1.size() != l2.size()) {
-      return false;
-    }
-
-    for (Object o : l1) {
-      if (!l2.contains(o)) {
-        return false;
-      }
-    }
-
-    for (Object o : l2) {
-      if (!l1.contains(o)) {
-        return false;
-      }
-    }
-
-    return true;
   }
 
   /**
@@ -89,6 +39,50 @@ public class CheckUtilities {
 
     if (null == obj) {
       throw new NullPointerException(ExceptionMessageMap.getMessage("000003")
+          + "  { variableName=[" + variableName + "] }");
+    }
+  }
+
+  /**
+   * Checks to see if two objects are equal. The Object.equal() method is used to check for
+   * equality.
+   * 
+   * @param o1
+   *          The first object to check.
+   * @param o2
+   *          The second object to check.
+   * @return True if the two objects are equal. False if the objects are not equal.
+   */
+  public static boolean checkObjectsEqual(Object o1, Object o2) {
+    if (null != o1 && !o1.equals(o2)) {
+      return false;
+    }
+  
+    if (null == o1 && null != o2) {
+      return false;
+    }
+  
+    return true;
+  }
+
+  /**
+   * Checks a <code>String</code> argument to make sure it is not null and contains one or more
+   * characters. If the <code>String</code> is null, a <code>NullPointerException</code> is
+   * thrown. If the <code>String</code> has length zero, an <code>IllegalArgumentException</code>
+   * is thrown.
+   * 
+   * @param str
+   *          The string to check.
+   * @param variableName
+   *          The name of the variable to use in throwing exceptions.
+   */
+  public static void checkStringArgument(String str, String variableName) {
+    if (null == str) {
+      throw new NullPointerException(ExceptionMessageMap.getMessage("000001")
+          + "  { variableName=[" + variableName + "] }");
+    }
+    if (str.length() == 0) {
+      throw new IllegalArgumentException(ExceptionMessageMap.getMessage("000001")
           + "  { variableName=[" + variableName + "] }");
     }
   }
@@ -116,40 +110,46 @@ public class CheckUtilities {
   }
 
   /**
-   * Checks a <code>String</code> argument to make sure it is not null and contains one or more
-   * characters. If the <code>String</code> is null, a <code>NullPointerException</code> is
-   * thrown. If the <code>String</code> has length zero, an <code>IllegalArgumentException</code>
-   * is thrown.
+   * Checks if two unordered lists are equal.
    * 
-   * @param str
-   *          The string to check.
-   * @param variableName
-   *          The name of the variable to use in throwing exceptions.
+   * @param l1
+   *          The first list to test.
+   * @param l2
+   *          The second list to test.
+   * @return True if:
+   *         <ul>
+   *         <li>both lists are null or</li>
+   *         <li>both lists are the same length, there exists an equivalent object in l2 for all
+   *         objects in l1, and there exists an equivalent object in l1 for all objects in l2</li>
+   *         </ul>
+   *         False otherwise.
    */
-  public static void checkStringArgument(String str, String variableName) {
-    if (null == str) {
-      throw new NullPointerException(ExceptionMessageMap.getMessage("000001")
-          + "  { variableName=[" + variableName + "] }");
+  public static boolean checkUnorderedListsEqual(List<?> l1, List<?> l2) {
+    if (null == l1 && null != l2) {
+      return false;
     }
-    if (str.length() == 0) {
-      throw new IllegalArgumentException(ExceptionMessageMap.getMessage("000001")
-          + "  { variableName=[" + variableName + "] }");
+  
+    if (null != l1 && null == l2) {
+      return false;
     }
-  }
-
-  /**
-   * Checks that the specified filename exists. This assumes that the above check for string
-   * validity has already been run and the path/filename is neither null or of size 0.
-   * 
-   * @param filename
-   *          File or directory path
-   */
-  public static void checkFileValidity(String filename) throws IOException {
-    File file = new File(filename);
-    if (!file.exists()) {
-      throw new IOException(ExceptionMessageMap.getMessage("020001") + "  { filename=[" + filename
-          + "] }");
+  
+    if (l1.size() != l2.size()) {
+      return false;
     }
+  
+    for (Object o : l1) {
+      if (!l2.contains(o)) {
+        return false;
+      }
+    }
+  
+    for (Object o : l2) {
+      if (!l1.contains(o)) {
+        return false;
+      }
+    }
+  
+    return true;
   }
 
 }
--- javagit/src/test/java/edu/nyu/cs/javagit/utilities/test/TestCheckUtilities.java
+++ javagit/src/test/java/edu/nyu/cs/javagit/utilities/test/TestCheckUtilities.java
@@ -22,6 +22,72 @@ public class TestCheckUtilities extends TestCase {
   }
 
   @Test
+  public void testCheckFileValidity() {
+    final String unixFileDir = "/tmp/";
+    final String windowsFileDir = "c:\\";
+    final String filename1 = "bababa.3223.5asdbbw3gni.gagwe";
+    final String filename2 = "baasdfbj65adaba.sdh3hs2s5y23.5asdbbw3gni.gagwe";
+  
+    String dirBase = null;
+    if (System.getProperty("os.name").contains("indows")) {
+      dirBase = windowsFileDir;
+    } else {
+      dirBase = unixFileDir;
+    }
+  
+    // Find a test filename that doesnot exist.
+    String fullFilePath = dirBase + filename1;
+    File file = new File(fullFilePath);
+    if (file.exists()) {
+      fullFilePath = dirBase + filename2;
+      file = new File(fullFilePath);
+      if (file.exists()) {
+        assertTrue(
+            "In checking for file validity, the two test filenames are already in existance.",
+            false);
+      }
+    }
+  
+    // Check for invalid case
+    assertCheckFileValidityIOException(fullFilePath,
+        "020001: File or path does not exist.  { filename=[" + fullFilePath + "] }");
+  
+    // Check for valid case
+    try {
+      if (!file.createNewFile()) {
+        assertTrue("Unable to create temporary file to test checkFileValidity", false);
+      }
+    } catch (IOException e) {
+      assertTrue("Unable to create temporary file to test checkFileValidity", false);
+    }
+    assertCheckFileValidityIsValid(fullFilePath);
+  
+    // Cleanup
+    // TODO (jhl388): If the test fails before this point, the file won't be removed! Fix it.
+    if (!file.delete()) {
+      assertTrue("Unable to delete temporary file after testing checkFileValidity", false);
+    }
+  }
+
+  private void assertCheckFileValidityIsValid(String filename) {
+    try {
+      CheckUtilities.checkFileValidity(filename);
+    } catch (IOException e) {
+      assertTrue("IOException thrown when one was not expected!", false);
+    }
+  }
+
+  private void assertCheckFileValidityIOException(String filename, String message) {
+    try {
+      CheckUtilities.checkFileValidity(filename);
+      assertTrue("IOException not thrown when one was expected!", false);
+    } catch (IOException e) {
+      assertEquals("IllegalArgumentException message not what was expected.", message, e
+          .getMessage());
+    }
+  }
+
+  @Test
   public void testCheckObjectEquals() {
     // Test for false results
     String s1 = "test me!";
@@ -41,35 +107,46 @@ public class TestCheckUtilities extends TestCase {
   }
 
   @Test
-  public void testCheckUnorderedListsEqual() {
-    // Test for false results
-    List<String> l1 = new ArrayList<String>();
-    List<String> l2 = null;
-    assertTrue(!CheckUtilities.checkUnorderedListsEqual(l1, l2));
+  public void testCheckStringArgument() {
+    assertCheckStringArgumentThrowsNPE(null, "aVariableName",
+        "000001: A String argument was not specified but is required.  "
+            + "{ variableName=[aVariableName] }");
+  
+    assertCheckStringArgumentThrowsIllegalArgException("", "aVariableName",
+        "000001: A String argument was not specified but is required.  "
+            + "{ variableName=[aVariableName] }");
+  
+    assertCheckStringArgumentIsValid("str", "SomeVarName");
+  }
 
-    l1 = null;
-    l2 = new ArrayList<String>();
-    assertTrue(!CheckUtilities.checkUnorderedListsEqual(l1, l2));
+  private void assertCheckStringArgumentIsValid(String str, String variableName) {
+    try {
+      CheckUtilities.checkStringArgument(str, variableName);
+    } catch (NullPointerException e) {
+      assertTrue("NPE exception thrown when one was not expected!", false);
+    } catch (IllegalArgumentException e) {
+      assertTrue("IllegalArgumentException exception thrown when one was not expected!", false);
+    }
+  }
 
-    l1 = new ArrayList<String>();
-    l1.add("str");
-    assertTrue(!CheckUtilities.checkUnorderedListsEqual(l1, l2));
+  private void assertCheckStringArgumentThrowsNPE(String str, String variableName, String message) {
+    try {
+      CheckUtilities.checkStringArgument(str, variableName);
+      assertTrue("NPE exception not thrown when one was expected!", false);
+    } catch (NullPointerException e) {
+      assertEquals("NPE did not contain the expected message.", message, e.getMessage());
+    }
+  }
 
-    String s = "st";
-    s += "r";
-    l2.add(s);
-    l2.add("Bing Bop");
-    assertTrue(!CheckUtilities.checkUnorderedListsEqual(l1, l2));
-
-    s = "Bing";
-    l1.add(s);
-    assertTrue(!CheckUtilities.checkUnorderedListsEqual(l1, l2));
-
-    // Test for true results
-    l1.remove(1);
-    s += " Bop";
-    l1.add(s);
-    assertTrue(CheckUtilities.checkUnorderedListsEqual(l1, l2));
+  private void assertCheckStringArgumentThrowsIllegalArgException(String str, String variableName,
+      String message) {
+    try {
+      CheckUtilities.checkStringArgument(str, variableName);
+      assertTrue("IllegalArgumentException not thrown when one was expected!", false);
+    } catch (IllegalArgumentException e) {
+      assertEquals("IllegalArgumentException did not contain the expected message.", message, e
+          .getMessage());
+    }
   }
 
   @Test
@@ -148,112 +225,35 @@ public class TestCheckUtilities extends TestCase {
   }
 
   @Test
-  public void testCheckStringArgument() {
-    assertCheckStringArgumentThrowsNPE(null, "aVariableName",
-        "000001: A String argument was not specified but is required.  "
-            + "{ variableName=[aVariableName] }");
-
-    assertCheckStringArgumentThrowsIllegalArgException("", "aVariableName",
-        "000001: A String argument was not specified but is required.  "
-            + "{ variableName=[aVariableName] }");
-
-    assertCheckStringArgumentIsValid("str", "SomeVarName");
-  }
-
-  private void assertCheckStringArgumentIsValid(String str, String variableName) {
-    try {
-      CheckUtilities.checkStringArgument(str, variableName);
-    } catch (NullPointerException e) {
-      assertTrue("NPE exception thrown when one was not expected!", false);
-    } catch (IllegalArgumentException e) {
-      assertTrue("IllegalArgumentException exception thrown when one was not expected!", false);
-    }
-  }
-
-  private void assertCheckStringArgumentThrowsNPE(String str, String variableName, String message) {
-    try {
-      CheckUtilities.checkStringArgument(str, variableName);
-      assertTrue("NPE exception not thrown when one was expected!", false);
-    } catch (NullPointerException e) {
-      assertEquals("NPE did not contain the expected message.", message, e.getMessage());
-    }
-  }
-
-  private void assertCheckStringArgumentThrowsIllegalArgException(String str, String variableName,
-      String message) {
-    try {
-      CheckUtilities.checkStringArgument(str, variableName);
-      assertTrue("IllegalArgumentException not thrown when one was expected!", false);
-    } catch (IllegalArgumentException e) {
-      assertEquals("IllegalArgumentException did not contain the expected message.", message, e
-          .getMessage());
-    }
-  }
-
-  @Test
-  public void testCheckFileValidity() {
-    final String unixFileDir = "/tmp/";
-    final String windowsFileDir = "c:\\";
-    final String filename1 = "bababa.3223.5asdbbw3gni.gagwe";
-    final String filename2 = "baasdfbj65adaba.sdh3hs2s5y23.5asdbbw3gni.gagwe";
-
-    String dirBase = null;
-    if (System.getProperty("os.name").contains("indows")) {
-      dirBase = windowsFileDir;
-    } else {
-      dirBase = unixFileDir;
-    }
-
-    // Find a test filename that doesnot exist.
-    String fullFilePath = dirBase + filename1;
-    File file = new File(fullFilePath);
-    if (file.exists()) {
-      fullFilePath = dirBase + filename2;
-      file = new File(fullFilePath);
-      if (file.exists()) {
-        assertTrue(
-            "In checking for file validity, the two test filenames are already in existance.",
-            false);
-      }
-    }
-
-    // Check for invalid case
-    assertCheckFileValidityIOException(fullFilePath,
-        "020001: File or path does not exist.  { filename=[" + fullFilePath + "] }");
-
-    // Check for valid case
-    try {
-      if (!file.createNewFile()) {
-        assertTrue("Unable to create temporary file to test checkFileValidity", false);
-      }
-    } catch (IOException e) {
-      assertTrue("Unable to create temporary file to test checkFileValidity", false);
-    }
-    assertCheckFileValidityIsValid(fullFilePath);
-
-    // Cleanup
-    // TODO (jhl388): If the test fails before this point, the file won't be removed! Fix it.
-    if (!file.delete()) {
-      assertTrue("Unable to delete temporary file after testing checkFileValidity", false);
-    }
-  }
-
-  private void assertCheckFileValidityIsValid(String filename) {
-    try {
-      CheckUtilities.checkFileValidity(filename);
-    } catch (IOException e) {
-      assertTrue("IOException thrown when one was not expected!", false);
-    }
-  }
-
-  private void assertCheckFileValidityIOException(String filename, String message) {
-    try {
-      CheckUtilities.checkFileValidity(filename);
-      assertTrue("IOException not thrown when one was expected!", false);
-    } catch (IOException e) {
-      assertEquals("IllegalArgumentException message not what was expected.", message, e
-          .getMessage());
-    }
+  public void testCheckUnorderedListsEqual() {
+    // Test for false results
+    List<String> l1 = new ArrayList<String>();
+    List<String> l2 = null;
+    assertTrue(!CheckUtilities.checkUnorderedListsEqual(l1, l2));
+  
+    l1 = null;
+    l2 = new ArrayList<String>();
+    assertTrue(!CheckUtilities.checkUnorderedListsEqual(l1, l2));
+  
+    l1 = new ArrayList<String>();
+    l1.add("str");
+    assertTrue(!CheckUtilities.checkUnorderedListsEqual(l1, l2));
+  
+    String s = "st";
+    s += "r";
+    l2.add(s);
+    l2.add("Bing Bop");
+    assertTrue(!CheckUtilities.checkUnorderedListsEqual(l1, l2));
+  
+    s = "Bing";
+    l1.add(s);
+    assertTrue(!CheckUtilities.checkUnorderedListsEqual(l1, l2));
+  
+    // Test for true results
+    l1.remove(1);
+    s += " Bop";
+    l1.add(s);
+    assertTrue(CheckUtilities.checkUnorderedListsEqual(l1, l2));
   }
 
 }
