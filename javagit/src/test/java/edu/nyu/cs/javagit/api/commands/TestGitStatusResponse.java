a954a309a964295489d638785454f9370a0ae424
gsd216 <gsd216@7bfa78df-70c8-4e7b-bb4f-2d2f1591e478>
2008-07-27 16:35:53 +0000

    Added more tests to check the parsing of different GitStatus outputs

--- javagit/src/test/java/edu/nyu/cs/javagit/api/commands/TestGitStatusResponse.java
+++ javagit/src/test/java/edu/nyu/cs/javagit/api/commands/TestGitStatusResponse.java
@@ -16,12 +16,16 @@
  */
 package edu.nyu.cs.javagit.api.commands;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
 import junit.framework.TestCase;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import edu.nyu.cs.javagit.api.commands.GitStatusResponse;
+import edu.nyu.cs.javagit.api.JavaGitException;
 import edu.nyu.cs.javagit.client.cli.CliGitStatus;
 import edu.nyu.cs.javagit.client.cli.CliGitStatus.GitStatusParser;
 
@@ -29,31 +33,13 @@ public class TestGitStatusResponse extends TestCase {
 
   GitStatusParser parser;
   CliGitStatus gitStatus;
-  
+
   @Before
-  public void setUp() throws Exception {   
+  public void setUp() throws Exception {
     parser = new GitStatusParser();
     gitStatus = new CliGitStatus();
   }
-  
-  @Test
-  public void testGitStatus() {
-    parser.parseLine("# On branch foo");
-    parser.parseLine("# Changes to be committed:");
-    parser.parseLine("#   (use \"git reset HEAD <file>...\" to unstage)");
-    parser.parseLine("#");
-    parser.parseLine("# new file:   foobar03");
-    parser.parseLine("#");
-    parser.parseLine("# Untracked files:");
-    parser.parseLine("#   (use \"git add <file>...\" to include in what will be committed)");
-    parser.parseLine("#");
-    parser.parseLine("# foobar01");
-    parser.parseLine("# foobar02");
 
-    GitStatusResponse response = parser.getResponse();
-    assertEquals("Branch Name", "foo", response.getBranch().getName());
-  }
-  
   @Test
   public void testModifiedFilePattern() {
     String line = "#  modified:   patttrn06";
@@ -66,7 +52,7 @@ public class TestGitStatusResponse extends TestCase {
     line = "# modified:   dir1/dir2/dir3/foobar07";
     assertTrue(CliGitStatus.Patterns.MODIFIED.matches(line));
   }
-  
+
   @Test
   public void testNewFilePattern() {
     String line = "#  new file:   foobar03";
@@ -80,7 +66,10 @@ public class TestGitStatusResponse extends TestCase {
     line = "#  new file:   dir1/dir2/dir3/foobar07";
     assertTrue(CliGitStatus.Patterns.NEW_FILE.matches(line));
   }
-  
+
+  /**
+   * Test for parsing lines that point to deleted files.
+   */
   @Test
   public void testDeletedFilePattern() {
     String line = "# deleted:    foobar03";
@@ -88,13 +77,16 @@ public class TestGitStatusResponse extends TestCase {
     line = "#       deleted:                  foobar03";
     assertTrue(CliGitStatus.Patterns.DELETED.matches(line));
     line = "# xyz deleted: pattern06";
-    assertFalse(CliGitStatus.Patterns.DELETED.matches(line));   
+    assertFalse(CliGitStatus.Patterns.DELETED.matches(line));
     line = "# deleted: xyz pattern06";
-    assertTrue(CliGitStatus.Patterns.DELETED.matches(line));  
+    assertTrue(CliGitStatus.Patterns.DELETED.matches(line));
     line = "#  deleted:   dir1/dir2/dir3/foobar07";
     assertTrue(CliGitStatus.Patterns.DELETED.matches(line));
   }
-  
+
+  /**
+   * Test for verifying lines that only contains a hash(#) in them.
+   */
   public void testEmptyHashLinePattern() {
     String line = "#";
     assertTrue(CliGitStatus.Patterns.EMPTY_HASH_LINE.matches(line));
@@ -103,7 +95,11 @@ public class TestGitStatusResponse extends TestCase {
     line = "# test";
     assertFalse(CliGitStatus.Patterns.EMPTY_HASH_LINE.matches(line));
   }
-  
+
+  /**
+   * Test for confirming that lines filenames are parsed correctly and correct filenames are
+   * extracted from the line.
+   */
   @Test
   public void testGetFilename() {
     String line = "# deleted:    foobar03";
@@ -112,5 +108,176 @@ public class TestGitStatusResponse extends TestCase {
     assertEquals("foobar04", parser.getFilename(line));
     line = "#  new file:   testDir1/foobar04";
     assertEquals("testDir1/foobar04", parser.getFilename(line));
+  }
+
+  /**
+   * Test for verifying the correct branch name in comment in <code>GitStatusResposne</code>
+   */
+  @Test
+  public void testNothingToCommit() {
+    GitStatusParser parser = new GitStatusParser();
+    parser.parseLine("# On branch master");
+    parser.parseLine("nothing to commit (working directory clean)");
+
+    GitStatusResponse response = parser.getResponse();
+    assertEquals("Branch name does not match", "master", response.getBranch().getName());
+    assertEquals("Status message does not match", "nothing to commit (working directory clean)",
+        response.getMessage());
+    assertEquals("No. of untracked files", 0, response.getUntrackedFilesSize());
+    assertEquals("No. of new filesToCommit", 0, response.getNewFilesToCommitSize());
+    assertEquals("No. of deletedFilesToCommit", 0, response.getDeletedFilesToCommitSize());
+    assertEquals("No. of modifiedFilesToCommit", 0, response.getModifiedFilesToCommitSize());
+    assertEquals("No. of deletedFilesNotUpdated", 0, response.getDeletedFilesNotUpdatedSize());
+    assertEquals("No. of modifiedFilesNotUpdated", 0, response.getModifiedFilesNotUpdatedSize());
+    assertEquals("No. of errors", 0, response.getErrorCount());
+  }
+
+  /**
+   * Test for verifying that correct no. of untracked files are created in the
+   * <code>GitStatusResponse</code> object when <code>GitStatus</code> is run.
+   */
+  @Test
+  public void testUntrackedFilesAndDircotories() {
+    GitStatusParser parser = new GitStatusParser();
+
+    parser.parseLine("# On branch master");
+    parser.parseLine("# Untracked files:");
+    parser.parseLine("#   (use \"git add <file>...\" to include in what will be committed)\"");
+    parser.parseLine("#");
+    parser.parseLine("# dir/");
+    parser.parseLine("# fileA");
+    parser.parseLine("# fileB");
+    parser.parseLine("# fileC");
+    parser.parseLine("nothing added to commit but untracked files present (use \"git add\" to track)");
+
+    GitStatusResponse response = parser.getResponse();
+    assertEquals("No. of untracked files", 4, response.getUntrackedFilesSize());
+    assertEquals("No. of new filesToCommit", 0, response.getNewFilesToCommitSize());
+    assertEquals("No. of deletedFilesToCommit", 0, response.getDeletedFilesToCommitSize());
+    assertEquals("No. of modifiedFilesToCommit", 0, response.getModifiedFilesToCommitSize());
+    assertEquals("No. of deletedFilesNotUpdated", 0, response.getDeletedFilesNotUpdatedSize());
+    assertEquals("No. of modifiedFilesNotUpdated", 0, response.getModifiedFilesNotUpdatedSize());
+    assertEquals("No. of errors", 0, response.getErrorCount());
+
+    Iterator<File> iter = response.getUntrackedFiles();
+    assertEquals("FileName", "dir", iter.next().getPath());
+    assertEquals("FileName", "fileA", iter.next().getPath());
+    assertEquals("FileName", "fileB", iter.next().getPath());
+    assertEquals("FileName", "fileC", iter.next().getPath());
+  }
+
+  /**
+   * Test verifies the names and no. of untracked files & new files that will be committed next time
+   * &lt;git-commit&gt; is run.
+   */
+  @Test
+  public void testNewlyAddedFiles() {
+    GitStatusParser parser = new GitStatusParser();
+    parser.parseLine("# On branch master");
+    parser.parseLine("# Changes to be committed:");
+    parser.parseLine("#   (use \"git reset HEAD <file>...\" to unstage)");
+    parser.parseLine("#");
+    parser.parseLine("# new file:   dir/fileD");
+    parser.parseLine("# new file:   fileA");
+    parser.parseLine("#");
+    parser.parseLine("# Untracked files:");
+    parser.parseLine("#   (use \"git add <file>...\" to include in what will be committed)");
+    parser.parseLine("#");
+    parser.parseLine("# fileB");
+    parser.parseLine("# fileC");
+
+    GitStatusResponse response = parser.getResponse();
+    assertEquals("Branch Name", "master", response.getBranch().getName());
+    assertEquals("No. of untracked files", 2, response.getUntrackedFilesSize());
+    assertEquals("No. of new filesToCommit", 2, response.getNewFilesToCommitSize());
+    assertEquals("No. of deletedFilesToCommit", 0, response.getDeletedFilesToCommitSize());
+    assertEquals("No. of modifiedFilesToCommit", 0, response.getModifiedFilesToCommitSize());
+    assertEquals("No. of deletedFilesNotUpdated", 0, response.getDeletedFilesNotUpdatedSize());
+    assertEquals("No. of modifiedFilesNotUpdated", 0, response.getModifiedFilesNotUpdatedSize());
+    assertEquals("No. of errors", 0, response.getErrorCount());
+
+    Iterator<File> iter = response.getNewFilesToCommitIterarator();
+    assertEquals("File Name", "dir/fileD", iter.next().getPath());
+    assertEquals("File Name", "fileA", iter.next().getPath());
+
+    iter = response.getUntrackedFiles();
+    assertEquals("File Name", "fileB", iter.next().getPath());
+    assertEquals("File Name", "fileC", iter.next().getPath());
+  }
+
+  /**
+   * Test simulates parsing an output generated by &lt;git-status&gt; command and verifies that no.
+   * of files parsed for newFilesToCommit, deletedFilesToCommit, deletedFilesNotUpdated and
+   * modifiedFilesNotUpdated are correct and match.
+   */
+  @Test
+  public void testModifiedAndDeletedFiles() {
+    GitStatusParser parser = new GitStatusParser();
+
+    parser.parseLine("# On branch master");
+    parser.parseLine("# Changes to be committed:");
+    parser.parseLine("#   (use \"git reset HEAD <file>...\" to unstage)");
+    parser.parseLine("#");
+    parser.parseLine("# new file:   dir/fileD");
+    parser.parseLine("# deleted:    fileC");
+    parser.parseLine("#");
+    parser.parseLine("# Changed but not updated:");
+    parser.parseLine("#   (use \"git add <file>...\" to update what will be committed)");
+    parser.parseLine("#");
+    parser.parseLine("# modified:   fileA");
+    parser.parseLine("# deleted:    fileB");
+    parser.parseLine("#");
+
+    GitStatusResponse response = parser.getResponse();
+    assertEquals("No. of untracked files", 0, response.getUntrackedFilesSize());
+    assertEquals("No. of new filesToCommit", 1, response.getNewFilesToCommitSize());
+    assertEquals("No. of deletedFilesToCommit", 1, response.getDeletedFilesToCommitSize());
+    assertEquals("No. of modifiedFilesToCommit", 0, response.getModifiedFilesToCommitSize());
+    assertEquals("No. of deletedFilesNotUpdated", 1, response.getDeletedFilesNotUpdatedSize());
+    assertEquals("No. of modifiedFilesNotUpdated", 1, response.getModifiedFilesNotUpdatedSize());
+    assertEquals("No. of errors", 0, response.getErrorCount());
+
+    assertEquals("FileName", "dir/fileD", response.getNewFilesToCommitIterarator().next().getPath());
+    assertEquals("FileName", "fileC", response.getDeletedFilesToCommitIterator().next().getPath());
+    assertEquals("FileName", "fileA", response.getModifiedFilesNotUpdatedIterator().next()
+        .getPath());
+    assertEquals("FileName", "fileB", response.getDeletedFilesNotUpdatedIterator().next().getPath());
+  }
+
+  /**
+   * Test for verifying that modified staged and un-staged files are parsed properly by
+   * <code>GitStatusParser</code>
+   */
+  @Test
+  public void testModifiedFilesFromCommandOutput() throws IOException, JavaGitException {
+
+    GitStatusParser parser = new GitStatusParser();
+
+    parser.parseLine("# On branch master");
+    parser.parseLine("# Changes to be committed:");
+    parser.parseLine("#   (use \"git reset HEAD <file>...\" to unstage");
+    parser.parseLine("#");
+    parser.parseLine("#       modified:   src/Modified.java");
+    parser.parseLine("#       deleted:    src/Removed.java");
+    parser.parseLine("#       deleted:    src/RemovedNotStaged.java");
+    parser.parseLine("#");
+    parser.parseLine("# Changed but not updated:");
+    parser.parseLine("#   (use \"git add <file>...\" to update what will be committed)");
+    parser.parseLine("#");
+    parser.parseLine("#       modified:   src/ModifiedNotStaged.java");
+    parser.parseLine("#");
+    parser.parseLine("# Untracked files:");
+    parser.parseLine("#   (use \"git add <file>...\" to include in what will be committed)");
+    parser.parseLine("#");
+    parser.parseLine("#       .classpath");
+    parser.parseLine("#       .project");
+    parser.parseLine("#       bin/");
+
+    GitStatusResponse response = parser.getResponse();
+    assertEquals(1, response.getModifiedFilesNotUpdatedSize());
+    assertEquals("ModifiedNotStaged.java", response.getModifiedFilesNotUpdatedIterator().next()
+        .getName());
+    assertEquals(1, response.getModifiedFilesToCommitSize());
+    assertEquals("Modified.java", response.getModifiedFilesToCommitIterator().next().getName());
   }
 }
