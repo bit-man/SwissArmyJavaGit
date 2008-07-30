8f6cb6352646453b5273fc2570072de149f8b54e
apj221 <apj221@7bfa78df-70c8-4e7b-bb4f-2d2f1591e478>
2008-07-30 08:54:58 +0000

    git log initial checkin - correcting svn errors

--- javagit/src/main/java/edu/nyu/cs/javagit/client/IGitLog.java
+++ javagit/src/main/java/edu/nyu/cs/javagit/client/IGitLog.java
@@ -16,11 +16,35 @@
  */
 package edu.nyu.cs.javagit.client;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import edu.nyu.cs.javagit.api.JavaGitException;
+import edu.nyu.cs.javagit.api.commands.GitLogOptions;
+import edu.nyu.cs.javagit.api.commands.GitLogResponse.Commit;
+
 /**
- * An interface to represent the git-log command.
+ * An interface to represent the git log command.
  */
 public interface IGitLog {
 
-  // TODO: Create method signatures for this interface.
+	/**
+	 * 
+	 * @param repositoryPath	
+	 *			A <code>File</code> instance for the path to the repository root (the parent
+	 *          directory of the .git directory) or a sub-directory in the working tree of the
+	 *          repository to move/rename against. This argument must represent the absolute path to 
+	 *          the desired directory as returned by the <code>File.getPath()</code> method. If null 
+	 *          is passed, a <code>NullPointerException</code> will be thrown.
+	 *
+	 * @param options	Specify option used to collect logs, see <code>GitLogOptions</code>
+	 * @return
+	 * @throws JavaGitException
+	 * 			Thrown when there is an error executing git log.
+	 * @throws IOException
+	 * 			Thrown when there is an error executing git log.
+	 */
+	public List<Commit> log(File repositoryPath, GitLogOptions options) throws JavaGitException, IOException;
 
 }
