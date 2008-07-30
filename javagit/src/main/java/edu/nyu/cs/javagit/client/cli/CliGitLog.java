20ed1669c13c10e95151ef1dd441b48c24a9703f
apj221 <apj221@7bfa78df-70c8-4e7b-bb4f-2d2f1591e478>
2008-07-30 08:53:57 +0000

    initial git log checkin, correcting svn errors

--- javagit/src/main/java/edu/nyu/cs/javagit/client/cli/CliGitLog.java
+++ javagit/src/main/java/edu/nyu/cs/javagit/client/cli/CliGitLog.java
@@ -16,13 +16,310 @@
  */
 package edu.nyu.cs.javagit.client.cli;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+
+import edu.nyu.cs.javagit.api.JavaGitException;
+import edu.nyu.cs.javagit.api.commands.CommandResponse;
+import edu.nyu.cs.javagit.api.commands.GitLogOptions;
+import edu.nyu.cs.javagit.api.commands.GitLogResponse;
+import edu.nyu.cs.javagit.api.commands.GitLogResponse.Commit;
 import edu.nyu.cs.javagit.client.IGitLog;
+import edu.nyu.cs.javagit.utilities.CheckUtilities;
 
 /**
  * Command-line implementation of the <code>IGitLog</code> interface.
  */
-public class CliGitLog implements IGitLog {
+public class CliGitLog implements IGitLog{
 
-  // TODO: Implement the required methods
+
+	/**
+	 * Implementations of &lt;git log&gt; with options and one file to be added to index.
+	 */
+	public List<Commit> log(File repositoryPath, GitLogOptions options)
+	throws JavaGitException, IOException {
+		CheckUtilities.checkFileValidity(repositoryPath);
+		GitLogParser parser = new GitLogParser();
+		List<String> command = buildCommand(repositoryPath, options);
+		GitLogResponse response =  (GitLogResponse) ProcessUtilities.runCommand(repositoryPath,
+				command, parser);
+		if (response.containsError()) {
+			int line = response.getError(0).getLineNumber();
+			String error = response.getError(0).error();
+			throw new JavaGitException(401001, "Line " + line + ", " + error);
+			//TODO verify exception number
+		}
+		return response.getLog();
+	}
+
+
+	/**
+	 * This function build the git log commands wih necessary options as specified by the user.
+	 * @param repositoryPath Root of the repository
+	 * @param options	Options supplied to the git log command using <code>GitLogOptions</code>.
+	 * @return Returns a List of command argument to be applied to git log.
+	 */
+	private List<String> buildCommand(File repositoryPath, GitLogOptions options) {
+		List<String> command = new ArrayList<String>();
+		command.add("git");
+		command.add("log");
+		if(options!=null){
+			//General Options
+			/**
+			 * Breaks rewrite changes in to pairs of delete and create.
+			 */
+			if (options.isOptBreakRewriteChanges()) {
+				command.add("-B");
+			}
+			/**
+			 * Detects renames
+			 */
+			if (options.isOptDetectRenames()) {
+				command.add("-M");
+			}
+			/**
+			 * Detects copies and renames, of original files
+			 */
+			if (options.isOptFindCopies()) {
+				command.add("-C");
+			}
+			/**
+			 * 	Detects copies and renames , very expensive operation.
+			 */
+			if (options.isOptFindCopiesHarder()) {
+				command.add("--find-copies-harder");
+			}
+			/**
+			 *  List details about lines modified and files affected in a commit.
+			 */
+			if (options.isOptFileDetails()) {
+				command.add("--numstat");
+			}
+			
+			/**
+			 * 	List all logs on the relative path.
+			 */
+			if (options.isOptRelative()) {
+				command.add("--relative="+options.getOptRelativePath().toString());
+			}
+			
+			/**
+			 * 	List all logs since specified date.
+			 */
+			if (options.isOptLimitCommitSince()) {
+				command.add("--since="+ options.getOptLimitSince().toString());
+			}
+			
+			/**
+			 * 	List all logs after specified date.
+			 */
+			if (options.isOptLimitCommitAfter()) {
+				command.add("--after="+ options.getOptLimitAfter().toString());
+			}
+			
+			/**
+			 * 	List all logs after specified date.
+			 */
+			if (options.isOptLimitCommitUntil()) {
+				command.add("--until="+ options.getOptLimitUntil().toString());
+			}
+			
+			/**
+			 * 	List all logs before specified date.
+			 */
+			if (options.isOptLimitCommitBefore()) {
+				command.add("--before="+ options.getOptLimitBefore().toString());
+			}
+			
+			/**
+			 * 	List all logs by an author
+			 */
+			if (options.isOptLimitAuthor()) {
+				command.add("--author="+ options.getOptAuthor().toString());
+			}
+			
+			/**
+			 * 	List all logs by an author/committer header pattern.
+			 */
+			if (options.isOptLimitCommitterPattern()) {
+				command.add("--committer="+ options.getOptLimitPattern().toString());
+			}
+			
+			/**
+			 * 	List all logs by matching to a grep pattern.
+			 */
+			if (options.isOptLimitGrep()) {
+				command.add("--grep="+ options.getOptLimitGrepPattern().toString());
+			}
+			/**
+			 * 	Match regular expressions with out  regard to letters case.
+			 */
+			if (options.isOptLimitMatchIgnoreCase()) {
+				command.add("-i");
+			}
+			
+			/**
+			 * 	Match extended regular expressions.
+			 */
+			if (options.isOptLimitEnableExtendedRegex()) {
+				command.add("-E");
+			}
+			
+			/**
+			 * 	Match patterns as fixed strings and not regular expressions.
+			 */
+			if (options.isOptLimitMatchIgnoreCase()) {
+				command.add("-F");
+			}
+			
+			/**
+			 * 	Stop when a path dissapears from the tree.
+			 */
+			if (options.isOptLimitRemoveEmpty()) {
+				command.add("--remove-empty");
+			}
+			
+			/**
+			 * 	Match parts of history irrelevant to the current path.
+			 */
+			if (options.isOptLimitFullHistory()) {
+				command.add("--full-history");
+			}
+			
+			/**
+			 * 	Do not print commits with more than one merges.
+			 */
+			if (options.isOptLimitNoMerges()) {
+				command.add("--no-merges");
+			}
+			
+			/**
+			 * 	Follow only first parent on seeing a merge.
+			 */
+			if (options.isOptLimitFirstParent()) {
+				command.add("--first-parent");
+			}
+			
+			/**
+			 * 	Order commits topologically.
+			 */
+			if (options.isOptOrderingTopological()) {
+				command.add("--topo-order");
+			}
+			
+			/**
+			 * 	Order commits in reverse
+			 */
+			if (options.isOptOrderingReverse()) {
+				command.add("--reverse");
+			}
+		}
+
+		return command;
+
+	}
+
+	/**
+	 * Parser class to parse the output generated by git log; and return a
+	 * <code>GitLogResponse</code> object.
+	 */
+	public class GitLogParser implements IParser {
+
+		int linesAdded = 0;
+		int linesDeleted = 0;
+		String filename = null;
+		private GitLogResponse response = new GitLogResponse();
+		boolean canCommit = false;
+		String []tmp ;
+		public CommandResponse getResponse() throws JavaGitException {
+			response.addCommit();
+			return this.response;
+		}
+
+		
+		public void parseLine(String line) {
+			if(line.length() == 0){
+				return;
+			}
+
+			//commit
+			if (line.startsWith("commit")){
+				if(canCommit){
+					response.addCommit();
+
+				}
+				canCommit = true;
+				tmp = line.split(" ");
+				response.setSha(line.substring(tmp[0].length()));
+			}
+
+			//merge (optional)
+			else if (line.startsWith("Merge")){
+				List<String> mergeDetails = new ArrayList<String>();
+				tmp = line.split(" ");
+				StringTokenizer st = new StringTokenizer(line.substring(tmp[0].length()));
+				while(st.hasMoreTokens()){
+					mergeDetails.add(st.nextToken());
+				}
+
+				response.setMergeDetails(mergeDetails);
+				mergeDetails = null;
+			}
+			//Author
+			else if (line.startsWith("Author")){
+				tmp = line.split(" ");
+				response.setAuthor(line.substring(tmp[0].length()));
+			}
+
+			//Date
+			else if (line.startsWith("Date")){
+				tmp = line.split(" ");
+				response.setDate(line.substring(tmp[0].length()));
+			}
+			//message or fileDetails
+			else{
+				StringTokenizer st = new StringTokenizer(line);
+				try{
+
+					linesAdded = Integer.parseInt(st.nextToken());
+					linesDeleted = Integer.parseInt(st.nextToken());
+					filename = st.nextToken().toString();
+					response.addFile(filename,linesAdded,linesDeleted);
+					
+				}
+				catch(NumberFormatException nfe){
+					try{
+					if (st.nextToken().equals("-")){
+						linesAdded = 0;
+						linesDeleted = 0;
+						filename = st.nextToken().toString();
+						response.addFile(filename,linesAdded,linesDeleted);
+					}
+					else{
+						response.setMessage(line);
+					}
+					}
+					catch(NoSuchElementException nsee){
+						response.setMessage(line);
+					}
+				}
+				catch(NoSuchElementException nsee){
+					response.setMessage(line);
+					
+				}
+				catch(Exception e){
+					response.setMessage(line);
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+
+	}
 
 }
