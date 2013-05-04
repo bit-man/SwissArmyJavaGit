e5b43c87e6fce5b6ce73ecfc7dfadaa830328a54
Víctor A. Rodríguez (Bit-Man) <victor@bit-man.com.ar>
2013-05-04 04:10:13 -0300

    Ignoring TestBase for JUnit test search

--- javagit/src/test/java/edu/nyu/cs/javagit/TestBase.java
+++ javagit/src/test/java/edu/nyu/cs/javagit/TestBase.java
@@ -5,6 +5,7 @@ import edu.nyu.cs.javagit.api.JavaGitException;
 import edu.nyu.cs.javagit.utilities.TestProperty;
 import junit.framework.TestCase;
 import org.junit.Before;
+import org.junit.Ignore;
 
 import java.io.IOException;
 
@@ -13,6 +14,7 @@ import java.io.IOException;
  * Date: 3/30/13
  * Time: 2:14 AM
  */
+@Ignore
 public class TestBase extends TestCase {
 
     @Before
