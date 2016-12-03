b3c4de01288cbad267b1b49a2dadba87fdac28d6
Víctor A. Rodríguez (Bit-Man) <victor@bit-man.guru>
2016-12-03 17:33:37 -0300

    Windows 7: How to setup GitPath? (issue #13)

--- javagit/src/test/java/edu/nyu/cs/javagit/api/TestJavaGitConfiguration.java
+++ javagit/src/test/java/edu/nyu/cs/javagit/api/TestJavaGitConfiguration.java
@@ -12,103 +12,119 @@ import java.io.IOException;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.fail;
 
-/**
- * Test cases for our <code>WorkingTree</code> class.
- */
-public class TestJavaGitConfiguration extends TestBase {
+public class TestJavaGitConfiguration
+        extends TestBase
+{
+
+    private String pathString;
+    private File pathFile;
+
+    @Test(expected = NullPointerException.class)
+    public void testSetNullPathShouldGenerateNPE()
+            throws IOException, JavaGitException
+    {
+        pathString = null;
+        JavaGitConfiguration.setGitPath(pathString);
+        fail("Invalid path - this should never be reached.");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetEmptyPathShouldGenerateIllegalArgumentException()
+            throws IOException, JavaGitException
+    {
+        pathString = "";
+        JavaGitConfiguration.setGitPath(pathString);
+        fail("Invalid path - this should never be reached.");
+    }
 
     @Test
-    public void testSettingGitPathExceptions() {
-        String pathString;
-        File pathFile;
-
-        // Attempting to set a null path should generate a NullPointerException.
-        pathString = null;
-        try {
-            JavaGitConfiguration.setGitPath(pathString);
-            fail("Invalid path - this should never be reached.");
-        } catch (NullPointerException e) {
-            // This is fine.
-        } catch (Exception e) {
-            fail("Null path (String) generated wrong kind of exception: " + e.getClass().getName());
-        }
-
-        // Attempting to set an empty path as a String should generate an IllegalArgumentException.
-        pathString = "";
-        try {
-            JavaGitConfiguration.setGitPath(pathString);
-            fail("Invalid path - this should never be reached.");
-        } catch (IllegalArgumentException e) {
-            // This is fine.
-        } catch (Exception e) {
-            fail("Empty path (String) generated wrong kind of exception: " + e.getClass().getName());
-        }
-
-        // Attempting to set an empty path as a File should generate an IOException.
+    public void testSetEmptyPathShouldGenerateIOException()
+    {
         pathFile = new File("");
-        try {
+        try
+        {
             JavaGitConfiguration.setGitPath(pathFile);
             fail("Invalid path - this should never be reached.");
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             // This is fine.
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             fail("Empty path (String) generated wrong kind of exception: " + e.getClass().getName());
         }
+    }
 
-        // Let's create a temp file and try to set that as the path. Should throw a JavaGitException.
-        try {
+    @Test
+    public void testSetFilePathInsteadOfFolderShouldGenerateJavaGitException()
+    {
+        try
+        {
             pathFile = File.createTempFile("xyz", null);
             JavaGitConfiguration.setGitPath(pathFile);
             fail("Invalid path - this should never be reached.");
-        } catch (JavaGitException e) {
+        } catch (JavaGitException e)
+        {
             assertEquals(e.getCode(), 020002);
             pathFile.delete();
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             fail("Non-directory path (File) generated wrong kind of exception: " + e.getClass().getName());
         }
+    }
 
-    /*
-     * Let's create a temp dir and try to set that as the path. It won't contain git, so it should
-     * throw a JavaGitException.
-     */
-        try {
+    @Test
+    public void testSetFilePathWothNoGitExecInsideItShouldGenerateJavaGitException()
+    {
+        try
+        {
             pathFile = FileUtilities.createTempDirectory("abc");
             JavaGitConfiguration.setGitPath(pathFile);
             fail("Invalid path - this should never be reached.");
-        } catch (JavaGitException e) {
+        } catch (JavaGitException e)
+        {
             assertEquals(e.getCode(), 100002);
             pathFile.delete();
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             fail("Temp path not containing git generated wrong kind of exception: "
                     + e.getClass().getName());
         }
+    }
 
+    @Test
+    public void testSetFilePathToNullWipesOutPreviouslySetGitPath()
+    {
     /*
      * Set the path using null as the File argument. This is valid - it's saying: wipe out whatever
      * was set before (via previous calls to setGitPath) and just look on the PATH for git.
      */
         pathFile = null;
-        try {
+        try
+        {
             JavaGitConfiguration.setGitPath(pathFile);
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             fail("Null path (File) generated exception: " + e.getClass().getName());
-        }
-
-        // Grab the version and see if we can get it exception-free.
-        String version = "";
-        try {
-            version = JavaGitConfiguration.getGitVersion();
-        } catch (Exception e) {
-            fail("Couldn't get git version string - saw \"" + version + "\" and got exception: "
-                    + e.getClass().getName());
         }
     }
 
-    public void testPathSeparator_issue2() throws TestErrorException {
-        try {
+    @Test
+    public void testGrabbingGitVersionShoudlBeExceptionF5ee()
+            throws JavaGitException
+    {
+        JavaGitConfiguration.getGitVersion();
+    }
+
+    @Test
+    public void testPathSeparator_issue2()
+            throws TestErrorException
+    {
+        try
+        {
             JavaGitConfiguration.setGitPath(TestProperty.GIT_PATH.asString());
             JavaGitConfiguration.getGitVersion();
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             throw new TestErrorException("May be you forgot to set property " + TestProperty.GIT_PATH.getName()
                     + "to a valid Git path ( value : '" + TestProperty.GIT_PATH.asString() + "' )", e);
         }
